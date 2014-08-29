c4d67dc09bf49bad102be09fcfc6efc0c6d44834
Alberto Bermejo Gil <abermejo@aubay.es>
2014-08-29 12:27:24 +0200

    FIX email phone missing

--- sendemail.php
+++ sendemail.php
@@ -5,15 +5,16 @@
 		'message'=>'Thank you for contact us. As early as possible  we will contact you '
 	);
 
-    $name = @trim(stripslashes($_POST['name'])); 
-    $email = @trim(stripslashes($_POST['email'])); 
-    $subject = @trim(stripslashes($_POST['subject'])); 
-    $message = @trim(stripslashes($_POST['message'])); 
+    $name = @trim(stripslashes($_POST['name']));
+    $email = @trim(stripslashes($_POST['email']));
+    $subject = @trim(stripslashes($_POST['subject']));
+    $message = @trim(stripslashes($_POST['message']));
+	$phone = @trim(stripslashes($_POST['phone']));
 
     $email_from = $email;
     $email_to = 'bermejo.a@gmail.com';//replace with your email
 
-    $body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
+    $body = 'Nombre: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Telefono: ' . $phone . "\n\n" . 'Asunto: ' . $subject . "\n\n" . 'Mensaje: ' . $message;
 
     $success = @mail($email_to, $subject, $body, 'From: <'.$email_from.'>');
 
